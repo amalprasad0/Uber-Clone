d5414df970760fbf3d32d1b6b53653b29bb9ed21
Amal Prasad <amalprasad354@gmail.com>
2023-06-10 15:05:38 +0530

    removed .env

--- components/Map.js
+++ components/Map.js
@@ -1,76 +1,92 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
-import MapView,{Marker} from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import tw from 'twrnc'
 import { useSelector } from 'react-redux'
-import { selectOrigin,selectDestination } from '../slices/navSlice' 
+import { selectOrigin, selectDestination } from '../slices/navSlice'
 import MapViewDirections from 'react-native-maps-directions'
-import {GOOGLE_MAPS_APIKEY} from '@env'
+import { GOOGLE_MAPS_APIKEY } from '@env'
 import { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { setTravelTimeInformation } from '../slices/navSlice'
 
 const Map = () => {
-  const origin =useSelector(selectOrigin)
-  const destination =useSelector(selectDestination)
-  const mapRef=useRef(null)
+  const origin = useSelector(selectOrigin)
+  const destination = useSelector(selectDestination)
+  const mapRef = useRef(null)
+  const dispatch = useDispatch()
   useEffect(() => {
-    if(!origin || !destination) return;
-    mapRef.current.fitToSuppliedMarkers(['origin','destination'],{
-      edgePadding:{top:100,right:100,bottom:100,left:100}
+    if (!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 100, right: 100, bottom: 100, left: 100 }
     })
-  }, [origin,destination])
+  }, [origin, destination])
+  useEffect(() => {
+    if (!origin || !destination) return;
+    const getTraveltime = async () => {
+      const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.rows[0].elements[0])
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]))
 
+        })
+    }
+    getTraveltime()
+  }, [origin, destination, GOOGLE_MAPS_APIKEY])
   return (
     <MapView
-    ref={mapRef}
-    style={tw`flex-1`}
-    mapType='mutedStandard'
-    initialRegion={{
-      latitude: origin.location.lat,
-      longitude: origin.location.lng,
-      latitudeDelta: 0.005,
-      longitudeDelta: 0.005,
-    }}
-  >
+      ref={mapRef}
+      style={tw`flex-1`}
+      mapType='mutedStandard'
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    >
       {origin && destination && (
         <MapViewDirections
-        origin={origin.description}
-        destination={destination.description}
-        apikey={GOOGLE_MAPS_APIKEY}
-        strokeWidth={3}
-        strokeColor='black'
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor='black'
 
         />
       )
 
       }
 
-    {origin?.location && (
-      <Marker
-      style={tw`bg-blue-400`}
-        coordinate={{
-          latitude: origin.location.lat,
-          longitude: origin.location.lng,
-        }}
-        title='Origin'
-        description={origin.description}
-        identifier='origin'
-      />
-    )
-  }
-        {destination?.location && (
-      <Marker
-      style={tw`bg-blue-400`}
-        coordinate={{
-          latitude: destination.location.lat,
-          longitude: destination.location.lng,
-        }}
-        title='Destination'
-        description={origin.description}
-        identifier='destination'
-      />
-    )
-        }
-  </MapView>
+      {origin?.location && (
+        <Marker
+          style={tw`bg-blue-400`}
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title='Origin'
+          description={origin.description}
+          identifier='origin'
+        />
+      )
+      }
+      {destination?.location && (
+        <Marker
+          style={tw`bg-blue-400`}
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title='Destination'
+          description={origin.description}
+          identifier='destination'
+        />
+      )
+      }
+    </MapView>
   )
 }
 
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -6,6 +6,9 @@ import { TouchableOpacity } from 'react-native'
 import tw from 'twrnc'
 import { useNavigation } from '@react-navigation/native'
 import { FlatList } from 'react-native-gesture-handler'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { selectTravelTimeInformation } from '../slices/navSlice'
 const data=[
   {
     id:"Uber-X-123",
@@ -26,18 +29,16 @@ const data=[
     image:"https://links.papareact.com/7pf"
   },
 ]
-
-
-
-
-
+const SURGE_CHARGE_RATE=1.5
 const RideOptionsCard = () => {
   const navigation=useNavigation()
+  const [selected, setselected]=useState(null)
+  const travelTimeInformation=useSelector(selectTravelTimeInformation)
   return (
     <View style={tw`bg-white  flex-grow`}>
       <View >
         <TouchableOpacity 
-        style={tw`absolute top-3 left-5 p-3 rounded-full`}
+        style={tw`absolute left-5 p-3 rounded-full`}
         onPress={()=>
           navigation.navigate("NavigateCard")
           // console.log("pressed")}
@@ -45,29 +46,53 @@ const RideOptionsCard = () => {
         >
           <Icon name="chevron-left" type="fontawesome"/>
         </TouchableOpacity>
-        <Text style={tw`text-center py-5 text-lg `}>Select a Ride</Text>
+        <Text style={tw`text-center py-1 text-lg `}>Select a Ride -{travelTimeInformation?.distance?.text} </Text>
       </View>
       <FlatList
       data={data}
       keyExtractor={(item)=>item.id}
       renderItem={({item:{id,title,multiplier,image},item})=>(
-        <TouchableOpacity style={tw`flex-row items-center justify-between px-10`}>
+        <TouchableOpacity 
+        onPress={()=>{
+          setselected(item)
+        }}
+        style={tw`flex-row items-center justify-between px-10 ${
+          id===selected?.id && "bg-gray-200"
+        }`}>
           <Image style={{
-            width:100,
-            height:100,
+            width:90,
+            height:90,
             resizeMode:"contain"
           }
           }
           source={{uri:image}}/>
           <View >
-            <Text>{title}</Text>
-            <Text>Travel time...</Text>
+            <Text style={tw`text-xl font-semibold`}>{title}</Text>
+            <Text>{travelTimeInformation?.duration?.text}</Text>
 
           </View>
-          <Text style={tw`text-xl`}>₹99.89/-</Text>
+          <Text style={tw`text-xl`}>₹
+          
+          {new Intl.NumberFormat('en-IN',{
+            style:"currency",
+            currency:"INR"
+          }).format(
+            (travelTimeInformation?.duration?.value * SURGE_CHARGE_RATE * multiplier)/100
+          )
+          }
+          </Text>
         </TouchableOpacity>
+
       )}
      />  
+     <View style={tw`mt-auto border-t border-gray-200`}>
+      <TouchableOpacity
+      disabled={!selected} style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-400"}`} >
+        <Text style={tw`text-center text-white text-lg`}>
+          Choose {selected?.title}
+        </Text>
+      </TouchableOpacity>
+     </View>
 
     </View>
   );
