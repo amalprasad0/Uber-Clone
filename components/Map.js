9b1ec9b8e73a37bcf37236b6de046031869218b8
Amal Prasad <amalprasad354@gmail.com>
2023-06-12 07:52:06 +0530

    removed .env

--- components/Map.js
+++ components/Map.js
@@ -39,62 +39,61 @@ const Map = () => {
     getTraveltime()
   }, [origin, destination, GOOGLE_MAPS_APIKEY])
   return (
-    // <MapView
-    //   ref={mapRef}
-    //   // style={tw`flex-1`}
-    //   style={styles.modalStyle}
-    //   mapType='mutedStandard'
-    //   initialRegion={{
-    //     latitude: origin.location.lat,
-    //     longitude: origin.location.lng,
-    //     latitudeDelta: 0.005,
-    //     longitudeDelta: 0.005,
-    //   }}
-    //   provider={PROVIDER_GOOGLE}
+    <MapView
+      ref={mapRef}
+      // style={tw`flex-1`}
+      style={styles.modalStyle}
+      mapType='mutedStandard'
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+      provider={PROVIDER_GOOGLE}
 
-    // >
-    //   {origin && destination && (
-    //     <MapViewDirections
-    //       origin={origin.description}
-    //       destination={destination.description}
-    //       apikey={GOOGLE_MAPS_APIKEY}
-    //       strokeWidth={3}
-    //       strokeColor='black'
+    >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor='black'
 
-    //     />
-    //   )
+        />
+      )
 
-    //   }
+      }
 
-    //   {origin?.location && (
-    //     <Marker
-    //       style={tw`bg-blue-400`}
-    //       coordinate={{
-    //         latitude: origin.location.lat,
-    //         longitude: origin.location.lng,
-    //       }}
-    //       title='Origin'
-    //       description={origin.description}
-    //       identifier='origin'
-    //     />
-    //   )
-    //   }
-    //   {destination?.location && (
-    //     <Marker
-    //       style={tw`bg-blue-400`}
-    //       coordinate={{
-    //         latitude: destination.location.lat,
-    //         longitude: destination.location.lng,
-    //       }}
-    //       title='Destination'
-    //       description={origin.description}
-    //       identifier='destination'
-    //     />
-    //   )
-    //   }
-    // </MapView>
-    <>
-    </>
+      {origin?.location && (
+        <Marker
+          style={tw`bg-blue-400`}
+          coordinate={{
+            latitude: origin.location.lat,
+            longitude: origin.location.lng,
+          }}
+          title='Origin'
+          description={origin.description}
+          identifier='origin'
+        />
+      )
+      }
+      {destination?.location && (
+        <Marker
+          style={tw`bg-blue-400`}
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title='Destination'
+          description={origin.description}
+          identifier='destination'
+        />
+      )
+      }
+    </MapView>
+    
   )
 }
 
